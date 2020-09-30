a519870944cfe57238ba4cdd2e3c102fc7546752
woodchucker <ditrafabri@gmail.com>
2020-10-01 01:42:56 +0200

    POM

--- src/main/java/com/example/postgresdemo/repository/QuestionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.postgresdemo.repository;
-
-import com.example.postgresdemo.model.Question;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface QuestionRepository extends JpaRepository<Question, Long> {
-}
